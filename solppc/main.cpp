97d4c5bf9a5bd56f9c59563c371e87118de59990
charles-liu <charles@vite.org>
2021-01-31 02:58:44 +0800

    Create minimal architecture

---
+++ solppc/main.cpp
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: GPL-3.0
+/**
+ * @author Charles Liu <charles@vite.org>
+ * @date 2021
+ * Solidity++ commandline compiler.
+ */
+
+#include "CommandLineInterface.h"
+#include <boost/exception/all.hpp>
+#include <clocale>
+#include <iostream>
+
+using namespace std;
+
+/*
+The equivalent of setlocale(LC_ALL, "C") is called before any user code is run.
+If the user has an invalid environment setting then it is possible for the call
+to set locale to fail, so there are only two possible actions, the first is to
+throw a runtime exception and cause the program to quit (default behaviour),
+or the second is to modify the environment to something sensible (least
+surprising behaviour).
+
+The follow code produces the least surprising behaviour. It will use the user
+specified default locale if it is valid, and if not then it will modify the
+environment the process is running in to use a sensible default. This also means
+that users do not need to install language packs for their OS.
+*/
+static void setDefaultOrCLocale()
+{
+#if __unix__
+	if (!std::setlocale(LC_ALL, ""))
+	{
+		setenv("LC_ALL", "C", 1);
+	}
+#endif
+}
+
+int main(int argc, char** argv)
+{
+	setDefaultOrCLocale();
+	solidity::frontend::CommandLineInterface cli;
+	if (!cli.parseArguments(argc, argv))
+		return 1;
+	if (!cli.processInput())
+		return 1;
+	bool success = false;
+	try
+	{
+		success = cli.actOnInput();
+	}
+	catch (boost::exception const& _exception)
+	{
+		cerr << "Exception during output generation: " << boost::diagnostic_information(_exception) << endl;
+		success = false;
+	}
+
+	return success ? 0 : 1;
+}
