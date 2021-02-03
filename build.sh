5c04508c4930c824e7a2286614e93d1bd2e0bf5d
charles-liu <charles@vite.org>
2021-02-03 15:39:18 +0800

    Add build script

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+
+ROOTDIR="$(dirname "$0")"
+BUILDDIR="${ROOTDIR}/build"
+
+if [[ $# -eq 0 ]]; then
+    BUILD_TYPE=Release
+else
+    BUILD_TYPE="$1"
+fi
+
+mkdir -p "${BUILDDIR}"
+cd "${BUILDDIR}"
+
+cmake .. -DCMAKE_BUILD_TYPE="$BUILD_TYPE" "${@:2}"
+make -j6
+
+if [[ "${CI}" == "" ]]; then
+	echo "Installing ..."
+	sudo make install
+fi
